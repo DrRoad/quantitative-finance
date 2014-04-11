62a4c0fd9a84d0f4d87c01a1dad776b31b647dea
Frederick Pelchat <frederick.pelchat@gmail.com>
2014-04-10 23:29:56 -0400

    pushing all in on gold
    How hard would you bust by pushing all in on GOLD (XAU) with a leverage
    of 50:1 and 500:1?

---
+++ gold.all.in.R
@@ -0,0 +1,14 @@
+library('xts')
+content <- url('http://dl.dropboxusercontent.com/s/jq18erl5n861b21/gold.daily.prices.csv?dl=1&token_hash=AAEa3HPQDxcQANILqG89Pv5dB_JelBMO1qM8HQPyqBL21A')
+gold.daily.prices <- read.csv(content, sep = '', stringsAsFactors = FALSE)
+idx <- as.Date(gold.daily.prices$DATE)
+prices <- (as.numeric(gold.daily.prices$VALUE))
+dat <- xts(prices, idx)
+p2p <- lag(dat, 1)
+gold.daily.prices <- merge.xts(dat, as.xts(p2p))
+colnames(gold.daily.prices) <- c('curr', 'prev')
+gold.daily.prices$mov <- ((gold.daily.prices$curr - gold.daily.prices$prev) / gold.daily.prices$prev) * 100
+bust.all.in.50.to.1 <- gold.daily.prices[gold.daily.prices$mov <= -2.0]
+bust.all.in.500.to.1 <- gold.daily.prices[gold.daily.prices$mov <= -0.2]
+tail(bust.all.in.50.to.1, 50)
+tail(bust.all.in.500.to.1, 50)
