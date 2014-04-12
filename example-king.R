709679472841595ecedf8798da5642530bcf8066
Frederick Pelchat <frederickpelchat@users.noreply.github.com>
2014-04-12 12:23:22 -0400

    make the code match the blog post
    make the code match the blog post

--- example-king.R
+++ example-king.R
@@ -1,6 +1,6 @@
 #Example: KING:NASDAQ; intraday 5 mins; last 2 days; candle chart
 source('intraday-data.R')
 ticker <- 'KING'
-intra <- f.get.google.intraday(ticker, 60 * 5, '2d')
-tail(intra, 10)
-candleChart(intra, multi.col = TRUE, theme = 'black')
+intra2 <- f.get.google.intraday(ticker, 60 * 5, '2d')
+tail(intra2, 10)
+candleChart(intra2, multi.col = TRUE, theme = 'black')
