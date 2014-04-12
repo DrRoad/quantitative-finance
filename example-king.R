a238189f2ca60b3cee691e7b774bb49679eee9c2
Frederick Pelchat <frederickpelchat@users.noreply.github.com>
2014-04-12 11:57:40 -0400

    code cleanup
    removed useless code
    removed example id
    comments on a single line

--- example-king.R
+++ example-king.R
@@ -1,8 +1,6 @@
-#Example 2
-#KING:NASDAQ; intraday 5 mins; last 2 days; candle chart
+#Example: KING:NASDAQ; intraday 5 mins; last 2 days; candle chart
 source('intraday-data.R')
 ticker <- 'KING'
-market <- 'NASDAQ'
-intra2 <- f.get.google.intraday(ticker, 60 * 5, '2d')
-tail(intra2, 10)
-candleChart(intra2, multi.col = TRUE, theme = 'black')
+intra <- f.get.google.intraday(ticker, 60 * 5, '2d')
+tail(intra, 10)
+candleChart(intra, multi.col = TRUE, theme = 'black')
